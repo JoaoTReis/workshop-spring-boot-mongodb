229af8d3e1e7720ebd6b610889b3ae30bc344934
Joao Reis <joaoteireis@gmail.com>
2024-11-18 13:53:14 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/joaoreis/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.joaoreis.workshopmongo.repository;
+
+import com.joaoreis.workshopmongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
