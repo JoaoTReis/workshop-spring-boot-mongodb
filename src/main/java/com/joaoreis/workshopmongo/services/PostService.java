56fc40080496f8348bfa64308dd02b386b392a91
Joao Reis <joaoteireis@gmail.com>
2024-12-05 13:03:01 -0300

    Consulta com vario criterios

--- src/main/java/com/joaoreis/workshopmongo/services/PostService.java
+++ src/main/java/com/joaoreis/workshopmongo/services/PostService.java
@@ -8,6 +8,7 @@ import com.joaoreis.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -26,4 +27,8 @@ public class PostService {
         return repo.searchTitle(text);
     }
 
+    public List<Post> fullSearch(String text, Date minDate, Date maxDate){
+        maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 *1000);
+        return repo.fullSearch(text, minDate, maxDate);
+    }
 }
