285cfa0bf655527f94b81468bde5a0d91e214642
Joao Reis <joaoteireis@gmail.com>
2024-12-05 11:51:58 -0300

    Consuta simples com @query

--- src/main/java/com/joaoreis/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/joaoreis/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.joaoreis.workshopmongo.repository;
 import com.joaoreis.workshopmongo.domain.Post;
 import com.joaoreis.workshopmongo.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,6 +11,9 @@ import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+
+    @Query("{ 'title': { $regex: ?0, $options: 'i'} }")
+    List<Post> searchTitle(String text);
 
     List<Post> findByTitleContainingIgnoreCase(String text);
 
